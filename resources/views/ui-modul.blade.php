5f68a7165c1590b6df1f53e3d46586e2ed8ac1d6
beizi.so <bezi.zhong@gmail.com>
2024-09-06 14:42:42 +0700

    -

--- resources/views/ui-modul.blade.php
+++ resources/views/ui-modul.blade.php
@@ -105,7 +105,7 @@
                 <!-- Footer -->
                     <footer class="content-footer footer bg-footer-theme">
                         <div class="container-fluid d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                            <div class="mb-2 mb-md-0"><a href="http://lpm.uvers.ac.id/" target="_blank" class="footer-link fw-semibold">SIMPRO </a>
+                            <div class="mb-2 mb-md-0"><a href="https://uvers.ac.id/" target="_blank" class="footer-link fw-semibold">UVERS </a>
                                 © <script>
                                 document.write(new Date().getFullYear())
                                 </script>
