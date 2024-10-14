a4352c48cf4009b92523a69825d176f46e785f77
beizi.so <bezi.zhong@gmail.com>
2024-10-14 16:27:33 +0700

    [updated] --comment preview lampiran

--- resources/views/general/laporan-proposal/preview-laporan-proposal.blade.php
+++ resources/views/general/laporan-proposal/preview-laporan-proposal.blade.php
@@ -218,7 +218,7 @@
             </table>
             
         </div>
-        <div class="page-break-lampiran">
+        {{-- <div class="page-break-lampiran">
             @if($data_lampiran->count() > 0)
             <h3 class="mb-3">Lampiran</h3>
                 @foreach($data_lampiran as $lampirans)
@@ -228,7 +228,7 @@
             @else
                 <p></p>
             @endif
-        </div>
+        </div> --}}
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
