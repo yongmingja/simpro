e8e78c8cf477770c6e99332e3a6581b7831d68cd
beizi.so <bezi.zhong@gmail.com>
2024-10-30 16:08:26 +0700

    [edit] login title

--- resources/views/auth/homepage.blade.php
+++ resources/views/auth/homepage.blade.php
@@ -7,7 +7,7 @@
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Login {{config('app.name')}}</title>
+    <title>Login Simpro</title>
 
     <meta name="description" content="" />
 
