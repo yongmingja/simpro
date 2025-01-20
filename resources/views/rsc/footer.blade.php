28534a776bd1278bd67e98e9459b9028a499cbec
beizi.so <bezi.zhong@gmail.com>
2025-01-20 15:20:43 +0700

    [fixed bugs] on collapse sidebar
    Problem: unable to click after collapse
    Solved: mismatch data-template, changed horizontal into vertical

--- resources/views/rsc/footer.blade.php
+++ resources/views/rsc/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-fluid d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-    <div class="mb-2 mb-md-0"><a href="http://lpm.uvers.ac.id/" target="_blank" class="footer-link fw-semibold">SIMPRO </a>
+    <div class="mb-2 mb-md-0"><a href="http://simpro.uvers.ac.id/" target="_blank" class="footer-link fw-semibold">SIMPRO </a>
         © <script>
         document.write(new Date().getFullYear())
         </script>
