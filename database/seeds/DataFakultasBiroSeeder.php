91bba1d2643a364e1dc89736fe3124d3deea83e8
Xiaonanhai <86953256+mrbeizi@users.noreply.github.com>
2025-05-22 15:36:30 +0700

    [revised] migration

--- database/seeds/DataFakultasBiroSeeder.php
+++ database/seeds/DataFakultasBiroSeeder.php
@@ -13,11 +13,11 @@ class DataFakultasBiroSeeder extends Seeder
     public function run()
     {
         $records = [
-            ['id' => '1','nama_fakultas_biro' => 'Bisnis','kode_fakultas_biro' => 'FEB','created_at' => now(), 'updated_at' => now()],
-            ['id' => '2','nama_fakultas_biro' => 'Komputer','kode_fakultas_biro' => 'FKOM','created_at' => now(), 'updated_at' => now()],
-            ['id' => '3','nama_fakultas_biro' => 'Seni','kode_fakultas_biro' => 'FS','created_at' => now(), 'updated_at' => now()],
-            ['id' => '4','nama_fakultas_biro' => 'Teknik','kode_fakultas_biro' => 'FT','created_at' => now(), 'updated_at' => now()],
-            ['id' => '5','nama_fakultas_biro' => 'Pendidikan, Bahasa dan Budaya','kode_fakultas_biro' => 'FPBB','created_at' => now(), 'updated_at' => now()],
+            ['id' => '1','nama_fakultas_biro' => 'Fakultas Bisnis','kode_fakultas_biro' => 'FEB','created_at' => now(), 'updated_at' => now()],
+            ['id' => '2','nama_fakultas_biro' => 'Fakultas Komputer','kode_fakultas_biro' => 'FKOM','created_at' => now(), 'updated_at' => now()],
+            ['id' => '3','nama_fakultas_biro' => 'Fakultas Seni','kode_fakultas_biro' => 'FS','created_at' => now(), 'updated_at' => now()],
+            ['id' => '4','nama_fakultas_biro' => 'Fakultas Teknik','kode_fakultas_biro' => 'FT','created_at' => now(), 'updated_at' => now()],
+            ['id' => '5','nama_fakultas_biro' => 'Fakultas Pendidikan, Bahasa dan Budaya','kode_fakultas_biro' => 'FPBB','created_at' => now(), 'updated_at' => now()],
             ['id' => '6','nama_fakultas_biro' => 'Kantor Admisi & Humas','kode_fakultas_biro' => 'KAH','created_at' => now(), 'updated_at' => now()],
             ['id' => '7','nama_fakultas_biro' => 'Kemahasiswaan','kode_fakultas_biro' => 'UCC','created_at' => now(), 'updated_at' => now()],
             ['id' => '8','nama_fakultas_biro' => 'UPT Sistem Informasi','kode_fakultas_biro' => 'UPT SI','created_at' => now(), 'updated_at' => now()]
